60202dc76edee2e80b17c8c94586cf17bad87bd7
Aaron <aarongabriel147@gmail.com>
2021-06-12 14:20:10 -0700

    form functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,21 +4,24 @@ import React, { useState, useEffect } from 'react';
 // special instructions - input text 
 // increase by 1 or decrease by 1 - quantity = type="number"
 
-
 export default function Form() {
 
     const [data, setData] = useState({
         size:"",
         sauce:"",
-        topping:"",
+        topping1: false,
+        topping2: false,
+        topping3: false,
+        topping4: false,
         instructions:"",
         name:"",
     })
-     
+
+// The ternary is checking the "type" and if it is "checkbox" it will return whatever the "checked" value is.
     const handleChange = e => {
-        // preventDefault();
-        const {name, value} = e.target
-        setData({ ...data, [name]: value})
+        const { name, value, checked, type } = e.target
+        const x = type === "checkbox" ? checked : value;
+        setData({ ...data, [name]: x})
     }
 
     return (
@@ -35,34 +38,33 @@ export default function Form() {
                 </label>
                 <label>
                     <p>Red Sauce</p>
-                    <input type="radio" value={data.sauce} name="sauce" onChange={handleChange} checked={data.sauce === "redSauce"}/>
+                    <input type="radio" value="a" name="sauce" onChange={handleChange} checked={data.sauce === "a"}/>
                     <p>Ranch Garlic</p>
-                    <input type="radio" value={data.sauce} name="sauce" onChange={handleChange} checked={data.sauce === "ranchGarlicSauce"}/>
+                    <input type="radio" value="b" name="sauce" onChange={handleChange} checked={data.sauce === "b"}/>
                     <p>Alfredo Sauce</p>
-                    <input type="radio" value={data.sauce} name="sauce" onChange={handleChange} checked={data.sauce === "alfredoSauce"}/>
+                    <input type="radio" value="c" name="sauce" onChange={handleChange} checked={data.sauce === "c"}/>
                 </label>
                 <label>
                     <h3>Add Toppings</h3>
-                    <p>Pepporoni</p>
-                    <input type="checkbox" />
+                    <p>Pepperoni</p>
+                    <input type="checkbox" onChange={handleChange} name="topping1" checked={data.topping1} />
                     <p>Olives</p>
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={handleChange} name="topping2" checked={data.topping2} />
                     <p>Mushrooms</p>
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={handleChange} name="topping3" checked={data.topping3}  />
                     <p>Bell Peppers</p>
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={handleChange} name="topping4" checked={data.topping4}  />
                 </label>
                 <label>
                     <h3>Special Instructions</h3>
-                    <input type="text" id="special-text" />
+                    <input type="text" id="special-text" onChange={handleChange} />
                 </label>
                 <label>
                     <h3>Enter your name.</h3>
-                    <input type="text" id="name-input" />
+                    <input type="text" id="name-input" onChange={handleChange} />
                 </label>
                 <button>Add to Order</button>
             </form>    
         </div>
     )
-
 }
