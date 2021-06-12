67ed31d066d9f184d998a984f9eab35a336326a9
Aaron <aarongabriel147@gmail.com>
2021-06-12 13:10:01 -0700

    ROUTING COMPLETE!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Form from "./components/Form";
 import Confirmation from "./components/Confirmation";
+import Home from "./components/Home";
+import { Link, Route } from 'react-router-dom';
 
 
 
@@ -11,9 +13,16 @@ function App() {
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
-      <Form />
-      <Confirmation />
-
+      {/* Route Links */}
+      <nav>
+          <Link to="/">Home</Link>
+          <Link to="/Form">Form</Link>
+          <Link to="/Confirmation">Confirmation</Link>
+      </nav>
+      {/* Routing to other components. */}
+      <Route exact path="/" component={Home} /> 
+      <Route path="/Form" component={Form} />
+      <Route path="/Confirmation" component={Confirmation} />
     </>
   );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+
+export default function Home() {
+
+    
+    return (
+        <>
+            <p>TESTING HOME</p>
+        </>
+    )
+}
